79aedaa23cfb4a4740e0da1c0daec06a2b798364
Hamza Ahmed <h.hamza011@gmail.com>
2019-05-09 03:36:49 +0500

    'init'

---
+++ index.js
@@ -0,0 +1,20 @@
+// var ele = document.getElementById("div");
+// console.log(ele.innerText.toLowerCase());
+// console.log(ele.innerText.toUpperCase());
+// console.log(ele.hasAttribute('class'));
+
+var para = document.getElementsByClassName("paragraph");
+// console.log(para[0].firstChild);
+console.log(para[0].lastChild);
+// for (var i = 0; i < para.length; i++){
+//     para[i].style.backgroundColor = 'red';
+// }
+// console.log(para[0].attributes.length);
+
+// para[0].innerHTML = '<b>Span</b>';
+// para[0].innerHTML.setAttribute("id")
+// var b = document.createElement("b");
+// var text = document.createTextNode("Hello");
+// console.log( typeof text);
+// b.innerText = 'Span';
+// para[0].appendChild(text);
